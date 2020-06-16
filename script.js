4ae337cbfc35836ba9bc9aa326a6358d585c911f
Trenton Schneider <trentonschneider@TrentonidersMBP.fios-router.home>
2020-06-16 16:35:28 -0400

    added stage transition and timer.

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-var timerEl = document.querySelector("runningTimer"),
-	timeLeft = 60,
-	score;
+var timerEl = document.getElementById("runningTimer"),
+	timeLeft = 0,
+	score,
+	beginning = document.getElementById("intro"),
+	middle = document.getElementById("quiz"),
+	end = document.getElementById("end"),
+	starter = document.querySelector("#start");
 
-function timer() {
-	var timerInterval = setInterval(function () {
-		secondsLeft--;
-		timerEl.textContent = timeLeft;
-	});
-}
+starter.addEventListener("click", function () {
+	beginning.classList.add("hide");
+	middle.classList.remove("hide");
+	timeLeft = 61;
+	function timer() {
+		var timerInterval = setInterval(function () {
+			timeLeft--;
+			timerEl.textContent = timeLeft;
+
+			if (timeLeft === 0) {
+				middle.classList.add("hide");
+				end.classList.remove("hide");
+				clearInterval(timerInterval);
+			}
+		}, 1000);
+	}
+	timer();
+});
