0119be4c8b3eda2f4c634d92d8b3f76a34637c42
Trent Schneider <trentonschneider@gmail.com>
2020-06-17 10:17:32 -0400

    Fixed score submit button issue.

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 var timerEl = document.getElementById("runningTimer"),
-	timeLeft = 5,
-	score,
-	beginning = document.getElementById("intro"),
-	middle = document.getElementById("quiz"),
-	end = document.getElementById("end"),
-	starter = document.querySelector("#start"),
-	submitScore = document.querySelector("#initials");
+  timeLeft = 5,
+  score,
+  beginning = document.getElementById("intro"),
+  middle = document.getElementById("quiz"),
+  end = document.getElementById("end"),
+  starter = document.querySelector("#start"),
+  submitScore = document.querySelector("#submit");
 
 starter.addEventListener("click", function () {
-	beginning.classList.add("hide");
-	middle.classList.remove("hide");
-	timeLeft = 61;
-	function timer() {
-		var timerInterval = setInterval(function () {
-			timeLeft--;
-			timerEl.textContent = timeLeft;
+  beginning.classList.add("hide");
+  middle.classList.remove("hide");
+  timeLeft = 5;
+  function timer() {
+    var timerInterval = setInterval(function () {
+      timeLeft--;
+      timerEl.textContent = timeLeft;
 
-			if (timeLeft === 0) {
-				middle.classList.add("hide");
-				end.classList.remove("hide");
-				clearInterval(timerInterval);
-			}
-		}, 1000);
-	}
-	timer();
+      if (timeLeft === 0) {
+        middle.classList.add("hide");
+        end.classList.remove("hide");
+        clearInterval(timerInterval);
+      }
+    }, 1000);
+  }
+  timer();
 });
 submitScore.addEventListener("click", function () {
-	event.preventDefault();
-	var initial = document.querySelector("#initials").value;
-	localStorage.setItem("initials", initial);
-	consol.log(initial + "" + timeLeft);
+  event.preventDefault();
+  var initial = document.querySelector("#initials").value;
+  localStorage.setItem("initials", initial);
+  console.log(initial);
 });
