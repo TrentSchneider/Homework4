fcc1297c9dedf26ebd8b2c6ec7f947f28ce9233a
Trent Schneider <trentonschneider@gmail.com>
2020-06-17 16:33:18 -0400

    Worked on inserting questions and pulling scores.

--- highScoreScript.js
+++ highScoreScript.js
@@ -1,3 +1,4 @@
 var userScore = document.querySelector("#scoreList");
 var listScore = JSON.parse(localStorage.getItem("initials"));
 userScore.textContent = listScore;
+console.log(initial);
--- script.js
+++ script.js
@@ -2,21 +2,25 @@ var timerEl = document.getElementById("runningTimer"),
   timeLeft = 5,
   score,
   beginning = document.getElementById("intro"),
-  middle = document.getElementById("quiz"),
+  middle = document.getElementById("stage2"),
   end = document.getElementById("end"),
   starter = document.querySelector("#start"),
-  submitScore = document.querySelector("#submit");
+  submitScore = document.querySelector("#submit"),
+  qSpace = document.getElementById("questionSpace"),
+  aSpace = document.getElementById("answerSpace"),
+  questionCount;
 
 starter.addEventListener("click", function () {
   beginning.classList.add("hide");
   middle.classList.remove("hide");
   timeLeft = 5;
+
   function timer() {
     var timerInterval = setInterval(function () {
       timeLeft--;
       timerEl.textContent = timeLeft;
 
-      if (timeLeft === 0) {
+      if (timeLeft === 0 || questionCount < quizInfo.length) {
         middle.classList.add("hide");
         end.classList.remove("hide");
         clearInterval(timerInterval);
@@ -24,6 +28,16 @@ starter.addEventListener("click", function () {
     }, 1000);
   }
   timer();
+  for (
+    let questionCount = 0;
+    questionCount < quizInfo.length;
+    questionCount++
+  ) {
+    console.log(quizInfo[questionCount].question);
+    var questionInsert = document.createElement("button");
+    questionInsert.textContent = quizInfo[questionCount].question;
+    qSpace.appendChild(questionInsert);
+  }
 });
 submitScore.addEventListener("click", function () {
   event.preventDefault();
