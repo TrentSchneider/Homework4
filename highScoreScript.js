c82a57ab5bc40bd8254111d5d3139b592f407da5
Trenton Schneider <trentonschneider@TrentonidersMBP.fios-router.home>
2020-06-19 19:50:41 -0400

    adjusted order of functions/variables to fix error

--- highScoreScript.js
+++ highScoreScript.js
@@ -1,33 +1,35 @@
 var userScore = document.querySelector("#scoreList"),
-  listScore = localStorage.getItem("highScore"),
-  listScoreArray = JSON.parse(listScore),
-  clearBtn = document.querySelector("#clear"),
-  returnBtn = document.querySelector("#return");
+	listScore = localStorage.getItem("highScore"),
+	listScoreArray = JSON.parse(listScore),
+	clearBtn = document.querySelector("#clear"),
+	returnBtn = document.querySelector("#return");
 
 clearBtn.addEventListener("click", function (event) {
-  event.preventDefault();
-  localStorage.clear();
-  userScore.innerHTML = "";
+	event.preventDefault();
+	localStorage.clear();
+	userScore.innerHTML = "";
 });
 returnBtn.addEventListener("click", function (event) {
-  event.preventDefault();
-  window.location.href = "index.html";
+	event.preventDefault();
+	window.location.href = "index.html";
 });
 
 // sorts the JSON array by score putting higher scores at the top
-function sortByProperty(property) {
-  return function (a, b) {
-    if (a[property] > b[property]) return -1;
-    else if (a[property] < b[property]) return 1;
-    return 0;
-  };
-}
-listScoreArray.sort(sortByProperty("score"));
+if (listScoreArray != null) {
+	function sortByProperty(property) {
+		return function (a, b) {
+			if (a[property] > b[property]) return -1;
+			else if (a[property] < b[property]) return 1;
+			return 0;
+		};
+	}
+	listScoreArray.sort(sortByProperty("score"));
 
-// adds each score to the high score page
-for (let i = 0; i < listScoreArray.length; i++) {
-  var insertScore = document.createElement("h5");
-  insertScore.textContent =
-    listScoreArray[i].initials + " " + listScoreArray[i].score;
-  userScore.appendChild(insertScore);
+	// adds each score to the high score page
+	for (let i = 0; i < listScoreArray.length; i++) {
+		var insertScore = document.createElement("h5");
+		insertScore.textContent =
+			listScoreArray[i].initials + " " + listScoreArray[i].score;
+		userScore.appendChild(insertScore);
+	}
 }
