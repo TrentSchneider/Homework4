fbe87b1caa65d16cda1d1eaa834917012ad26899
Trenton Schneider <trentonschneider@TrentonidersMBP.fios-router.home>
2020-06-19 22:28:58 -0400

    Replaced filler questions with actual questions

--- quiz.js
+++ quiz.js
@@ -1,27 +1,48 @@
 var quizInfo = [
-  {
-    question: "question 1",
-    choices: ["answer A", "answer B", "answer C", "answer D"],
-    answer: "answer A",
-  },
-  {
-    question: "question 2",
-    choices: ["answer A", "answer B", "answer C", "answer D"],
-    answer: "answer B",
-  },
-  {
-    question: "question 3",
-    choices: ["answer A", "answer B", "answer C", "answer D"],
-    answer: "answer C",
-  },
-  {
-    question: "question 4",
-    choices: ["answer A", "answer B", "answer C", "answer D"],
-    answer: "answer D",
-  },
-  {
-    question: "question 5",
-    choices: ["answer A", "answer B", "answer C", "answer D"],
-    answer: "answer A",
-  },
+	{
+		question: "What does DOM stand for?",
+		choices: [
+			"Document Object Model",
+			"Dynamic Object Markup",
+			"Dominant Orientation Mapping",
+			"Dominos",
+		],
+		answer: "Document Object Model",
+	},
+	{
+		question: "What is a function",
+		choices: [
+			"The ability for a page to be stored on a server",
+			"A block of code designed to perform a particular task",
+			"This is an issue that occurs in JS code",
+			"A gathering of different JS files",
+		],
+		answer: "A block of code designed to perform a particular task",
+	},
+	{
+		question:
+			"True or False: A JS file only needs to reside in the same folder as an HTML file to be used, it does not need to be referenced within the HTML file.",
+		choices: ["True", "False"],
+		answer: "False",
+	},
+	{
+		question: "What can JS DOM do on a page?",
+		choices: [
+			"It can add/change HTML",
+			"It can add/change CSS",
+			"It can remove elements from a page",
+			"All of the above",
+		],
+		answer: "All of the above",
+	},
+	{
+		question: "What are loops?",
+		choices: [
+			"It executes a block of code a number of times",
+			"It ties two JS files together",
+			"It allows the user to go back and forth between HTML pages",
+			"A way to declare variables",
+		],
+		answer: "It executes a block of code a number of times",
+	},
 ];
